09d0f8e5ea8d8be9d13efc793866991485a04f5d
Efrain Eduardo Visconti Vivas <eviscont@c3r2s1.42urduliz.com>
2024-03-07 14:06:49 +0100

    push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 14:03:14 by eviscont          #+#    #+#             */
+/*   Updated: 2024/03/07 14:04:50 by eviscont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+typedef struct s_vec
+{
+	int	x;
+	int	y;
+}	t_vec;
+
+#endif
