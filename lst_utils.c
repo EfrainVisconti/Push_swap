b9b08f6b4e2c964979e770c685041c11ecb71ef8
Efrain Eduardo Visconti Vivas <eviscont@c3r1s6.42urduliz.com>
2024-04-30 19:25:40 +0200

    auxiliar functions for lists

---
+++ lst_utils.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 15:14:21 by eviscont          #+#    #+#             */
+/*   Updated: 2024/04/30 18:56:29 by eviscont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+//Crea un nuevo nodo, inicializando value con el número dado
+t_stack	*new_node(int content)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		ft_error();
+	new->value = content;
+	new->next = NULL;
+	return (new);
+}
+
+//Dado un puntero al primer elemento de la lista, devuelve un puntero al ultimo
+t_stack	*last_node(t_stack *lst)
+{
+	while (lst->next != 0)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+//Agrega un nodo al final de la lista dada, incluso si la lista esta vacia
+void	add_back_node(t_stack **lst, t_stack *new)
+{
+	if (lst)
+	{
+		if (!*lst)
+			*lst = new;
+		else
+			last_node(*lst)->next = new;
+	}
+}
+
+//Devuelve el tamaño del stack
+int	stack_size(t_stack *lst)
+{
+	int	count;
+
+	count = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		count++;
+	}
+	return (count);
+}
+
+//Agrega un nodo al inicio de la lista
+void	add_front_node(t_list **lst, t_list *new)
+{
+	if (lst)
+	{
+		if (*lst)
+		{
+			new->next = *lst;
+		}
+		*lst = new;
+	}
+}
