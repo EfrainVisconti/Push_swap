4f659599b1e57725f7c2dae03b28c00ef13ce044
Efrain Eduardo Visconti Vivas <eviscont@c3r1s6.42urduliz.com>
2024-04-30 19:34:10 +0200

    program structure 2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:58:07 by eviscont          #+#    #+#             */
-/*   Updated: 2024/04/30 19:21:22 by eviscont         ###   ########.fr       */
+/*   Updated: 2024/04/30 19:32:40 by eviscont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,6 @@ void	ft_error(void)
 {
 	ft_printf("Error\n");
 	exit (0);
-}
-
-int	ft_isvalid(int c)
-{
-	if ((c >= 58 && c <= 126) || (c >= 33 && c <= 42)
-		|| (c == 44) || (c == 46) || (c == 47))
-		return (0);
-	return (1);
-}
-
-int	check_args(char **argv, int i, int j)
-{
-	while (argv[i] != NULL)
-	{
-		j = 0;
-		while ((argv[i][j] != '\0'))
-		{
-			if (argv[i][j] == '+' || argv[i][j] == '-')
-			{
-				if (!ft_isdigit(argv[i][++j]))
-					return (0);
-			}
-			else if (ft_isdigit(argv[i][j]))
-			{
-				j++;
-				if (argv[i][j] == '\0')
-					break ;
-				if ((!ft_isdigit(argv[i][j])) && argv[i][j] != ' ')
-					return (0);
-			}
-			if (!ft_isvalid(argv[i][j]))
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	return (1);
 }
 
 void	stack_init(char **argv, t_stack **a, int i)
