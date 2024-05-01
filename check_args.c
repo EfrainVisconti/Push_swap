0a5b298f70cb73f7b84f395c22e99cf0f6b783a7
Efrain Eduardo Visconti Vivas <eviscont@c3r1s6.42urduliz.com>
2024-05-01 15:17:19 +0200

    check_args.c edit

--- check_args.c
+++ check_args.c
@@ -6,13 +6,33 @@
 /*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 19:30:28 by eviscont          #+#    #+#             */
-/*   Updated: 2024/04/30 19:31:28 by eviscont         ###   ########.fr       */
+/*   Updated: 2024/05/01 15:12:52 by eviscont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_isvalid(int c)
+int	check_duplicates(char **argv, int i)
+{
+	int j;
+	
+	if (*argv[i] == '\0')
+		return (0);
+	while (argv[i] != NULL)
+	{
+		j = i + 1;
+		while (argv[j] != NULL)
+		{
+			if (ft_atoi_push(argv[i], argv) == ft_atoi_push(argv[j], argv))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_isvalid(int c)
 {
 	if ((c >= 58 && c <= 126) || (c >= 33 && c <= 42)
 		|| (c == 44) || (c == 46) || (c == 47))
