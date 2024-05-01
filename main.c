846907f3b04528c9141b56a1da4e297885ed8210
Efrain Eduardo Visconti Vivas <eviscont@c3r1s6.42urduliz.com>
2024-05-01 15:16:17 +0200

    main.c edit

--- main.c
+++ main.c
@@ -6,25 +6,22 @@
 /*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:58:07 by eviscont          #+#    #+#             */
-/*   Updated: 2024/04/30 19:32:40 by eviscont         ###   ########.fr       */
+/*   Updated: 2024/05/01 14:53:26 by eviscont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_error(void)
-{
-	ft_printf("Error\n");
-	exit (0);
-}
-
-void	stack_init(char **argv, t_stack **a, int i)
+void	stack_init(char **argv, int argc, t_stack **a, int i)
 {
 	while (argv[i] != NULL)
 	{
-		add_back_node(a, new_node(ft_atoi(argv[i])));
+		if (argv)
+		add_back_node(a, new_node(ft_atoi_push(argv[i], argv)));
 		i++;
 	}
+	if (argc == 2)
+		free_array(argv);
 }
 
 void	quoted_parse(char *argv, t_stack **a)
@@ -32,7 +29,12 @@ void	quoted_parse(char *argv, t_stack **a)
 	char	**temp;
 
 	temp = ft_split(argv, 32);
-	stack_init(temp, a, 0);
+	if (check_duplicates(temp, 0) == 0)
+	{
+		free_array(temp);
+		ft_error();
+	}
+	stack_init(temp, 2, a, 0);
 }
 
 int	main(int argc, char **argv)
@@ -49,11 +51,12 @@ int	main(int argc, char **argv)
 	}
 	if (argc > 2)
 	{
-		if (check_args(argv, 1, 0) == 0)
+		if (check_args(argv, 1, 0) == 0 || check_duplicates(argv, 1) == 0)
 			ft_error();
 		else
-			stack_init(argv, &a, 1);
+			stack_init(argv, argc, &a, 1);
 	}
 	print_aux(a);
+	free_stack(&a);
 	return (0);
 }
